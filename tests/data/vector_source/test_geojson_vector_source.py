225745b9e296d2e10d432b499cf8f8506bf8ddea
Lewis Fishgold <lewfish@gmail.com>
2019-03-07 14:35:00 -0500

    wip

--- tests/data/vector_source/test_geojson_vector_source.py
+++ tests/data/vector_source/test_geojson_vector_source.py
@@ -13,70 +13,58 @@ class TestGeoJSONVectorSource(unittest.TestCase):
     """This also indirectly tests the ClassInference class."""
 
     def setUp(self):
-        self.class_map = ClassMap.construct_from(['building', 'car'])
-        self.class_id_to_filter = {
-            1: ['==', 'type', 'building'],
-            2: ['any', ['==', 'type', 'car'], ['==', 'type', 'auto']]
-        }
-        self.default_class_id = 4
-        self.point = {'type': 'Point', 'coordinates': [1, 1]}
         self.temp_dir = RVConfig.get_tmp_dir()
         self.uri = os.path.join(self.temp_dir.name, 'vectors.json')
 
     def tearDown(self):
         self.temp_dir.cleanup()
 
-    def get_inferred_class_ids(self, default_class_id):
-        # This should hit the 4 ways of inferring a class_id.
-        geojson = {
+    def props_to_geojson(self, props):
+        return {
             'type':
             'FeatureCollection',
             'features': [{
-                'properties': {
-                    'class_id': 3
-                },
-                'geometry': self.point
-            }, {
-                'properties': {
-                    'label': 'car'
-                },
-                'geometry': self.point
-            }, {
-                'properties': {
-                    'type': 'auto'
-                },
-                'geometry': self.point
-            }, {'geometry': self.point}]
+                'properties': props,
+                'geometry': {'type': 'Point', 'coordinates': [1, 1]}
+            }]
         }
+
+    def _test_class_inf(self, props, exp_class_ids, default_class_id=None):
+        geojson = self.props_to_geojson(props)
         json_to_file(geojson, self.uri)
 
+        class_map = ClassMap.construct_from(['building', 'car', 'tree'])
+        class_id_to_filter = {
+            1: ['==', 'type', 'building'],
+            2: ['any', ['==', 'type', 'car'], ['==', 'type', 'auto']]
+        }
         b = GeoJSONVectorSourceConfigBuilder() \
-            .with_class_inference(
-                class_id_to_filter=self.class_id_to_filter,
-                default_class_id=default_class_id) \
+            .with_class_inference(class_id_to_filter=class_id_to_filter,
+                                  default_class_id=default_class_id) \
             .with_uri(self.uri) \
             .build()
-
         msg = b.to_proto()
         config = GeoJSONVectorSourceConfig.from_proto(msg)
         source = config.create_source(
-            crs_transformer=IdentityCRSTransformer(), class_map=self.class_map)
-
+            crs_transformer=IdentityCRSTransformer(), class_map=class_map)
         trans_geojson = source.get_geojson()
         class_ids = [f['properties']['class_id'] for f in trans_geojson['features']]
-        return class_ids
-
-    def test_class_inf(self):
-        class_ids = self.get_inferred_class_ids(self.default_class_id)
-        exp_class_ids = [3, 2, 2, self.default_class_id]
         self.assertEqual(class_ids, exp_class_ids)
 
-    def test_class_inf_drop_none(self):
-        """Test that it drops features when no default_class_id."""
-        default_class_id = None
-        class_ids = self.get_inferred_class_ids(default_class_id)
-        exp_class_ids = [3, 2, 2]
-        self.assertEqual(class_ids, exp_class_ids)
+    def test_class_inf_class_id(self):
+        self._test_class_inf({'class_id': 3}, [3])
+
+    def test_class_inf_label(self):
+        self._test_class_inf({'label': 'car'}, [2])
+
+    def test_class_inf_filter(self):
+        self._test_class_inf({'type': 'auto'}, [2])
+
+    def test_class_inf_default(self):
+        self._test_class_inf({}, [4], default_class_id=4)
+
+    def test_class_inf_no_default(self):
+        self._test_class_inf({}, [])
 
     def test_transform_geojson(self):
         geojson = {
